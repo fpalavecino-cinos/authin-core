8d0ab43c4b51de04ddaa63b289c96babbcd3baef
palavecinofranco <francopalavecinoagus@gmail.com>
2025-08-04 14:30:59 -0300

    add notifications

--- src/main/java/org/cinos/core/posts/service/impl/StorageService.java
+++ src/main/java/org/cinos/core/posts/service/impl/StorageService.java
@@ -1,6 +1,8 @@
 package org.cinos.core.posts.service.impl;
 
 import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.BlobId;
+import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Bucket;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
@@ -10,8 +12,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @RequiredArgsConstructor
@@ -31,9 +35,9 @@ public class StorageService {
             String fileName = System.currentTimeMillis() + "_" + file.getOriginalFilename();
             Bucket bucket = storage.get(bucketName);
             Blob blob = bucket.create(fileName, file.getBytes());
-            // Generar URL pública en lugar de URL que requiere autenticación
-            String publicUrl = generatePublicUrl(fileName);
-            fileUrls.add(publicUrl);
+            // Generar URL firmada que funciona sin configuración pública
+            String signedUrl = generateSignedUrl(fileName);
+            fileUrls.add(signedUrl);
         }
         return fileUrls;
     }
@@ -42,8 +46,8 @@ public class StorageService {
         String fileName = System.currentTimeMillis() + "_" + file.getOriginalFilename();
         Bucket bucket = storage.get(bucketName);
         Blob blob = bucket.create(fileName, file.getBytes());
-        // Generar URL pública en lugar de URL que requiere autenticación
-        return generatePublicUrl(fileName);
+        // Generar URL firmada que funciona sin configuración pública
+        return generateSignedUrl(fileName);
     }
 
     public byte[] downloadFile(String fileName) throws IOException {
@@ -52,11 +56,21 @@ public class StorageService {
     }
 
     /**
-     * Genera una URL pública para el archivo en Google Cloud Storage
+     * Genera una URL firmada que funciona sin configuración pública del bucket
      * @param fileName nombre del archivo
-     * @return URL pública del archivo
+     * @return URL firmada del archivo
      */
-    private String generatePublicUrl(String fileName) {
-        return String.format("https://storage.googleapis.com/%s/%s", bucketName, fileName);
+    private String generateSignedUrl(String fileName) {
+        try {
+            BlobId blobId = BlobId.of(bucketName, fileName);
+            BlobInfo blobInfo = BlobInfo.newBuilder(blobId).build();
+            
+            // Generar URL firmada válida por 1 hora
+            URL signedUrl = storage.signUrl(blobInfo, 1, TimeUnit.HOURS);
+            return signedUrl.toString();
+        } catch (Exception e) {
+            // Fallback a URL pública si hay error
+            return String.format("https://storage.googleapis.com/%s/%s", bucketName, fileName);
+        }
     }
 }
